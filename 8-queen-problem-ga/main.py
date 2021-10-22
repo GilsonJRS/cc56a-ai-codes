d9e3ed72756eadff6cc5236477b6b00356f67951
Luis Eduardo Siman <siman@alunos.utfpr.edu.br>
2021-10-22 19:48:40 -0300

    Add function to print the succesfull board

--- 8-queen-problem-ga/main.py
+++ 8-queen-problem-ga/main.py
@@ -4,36 +4,52 @@ import numpy as np
 
 STOP_FLAG = False
 
+
 def fitness(chromosome):
     global STOP_FLAG
     attacks = abs(len(chromosome) - len(np.unique(chromosome)))
     for i in range(len(chromosome)):
         for j in range(len(chromosome)):
-            if(i!=j):
+            if(i != j):
                 dx = abs(i-j)
                 dy = abs(chromosome[i]-chromosome[j])
                 if(dx == dy):
-                    attacks+=1
+                    attacks += 1
     if(attacks == 0):
         print(chromosome)
+        printBoard(chromosome)
         STOP_FLAG = True
     return 28 - attacks
+
 
 def getPopulation(size):
     population = []
     for _ in range(size):
-        population.append(random.sample(range(1,9), 8))
+        population.append(random.sample(range(1, 9), 8))
     return population
 
-def reproduce(x,y):
-    c = random.randint(0,7)
+
+def reproduce(x, y):
+    c = random.randint(0, 7)
     child = x[0:c]+y[c:8]
     return child
+
 
 def mutate(child, mutprob):
     for i in range(len(child)):
         if(random.random() < mutprob):
-            child[i] = random.randint(1,8)
+            child[i] = random.randint(1, 8)
+
+
+def printBoard(chromosome):
+    for i in range(8):
+        for j in range(8):
+            if(chromosome[j] == i+1):
+                print("X ", end='')
+            else:
+                print(". ", end='')
+        print("\n")
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--maxiter', type=int, default=100000)
@@ -52,8 +68,8 @@ for i in range(args.maxiter):
         x = random.choices(population, weights=fit_prob)[0]
         y = random.choices(population, weights=fit_prob)[0]
         while(y == x):
-           y = random.choices(population, weights=fit_prob)[0]
-        child = reproduce(x,y)
+            y = random.choices(population, weights=fit_prob)[0]
+        child = reproduce(x, y)
         mutate(child, args.mutprob)
         new_pop.append(child)
     population = new_pop.copy()
