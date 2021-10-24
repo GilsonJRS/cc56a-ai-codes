14ad651f4dd3e82b31817c173ab7ce795e7e2f2c
GilsonJRS <gilsonjuniorcva@gmail.com>
2021-10-24 17:39:44 -0300

    corrections

--- 8-queen-problem-ga/main.py
+++ 8-queen-problem-ga/main.py
@@ -2,10 +2,8 @@ import random
 import argparse
 import numpy as np
 
-STOP_FLAG = False
-
+#return de fitness of a member in the population
 def fitness(chromosome, n_queen):
-    global STOP_FLAG
     attacks = abs(len(chromosome) - len(np.unique(chromosome)))
     for i in range(len(chromosome)):
         for j in range(len(chromosome)):
@@ -15,37 +13,44 @@ def fitness(chromosome, n_queen):
                 if(dx == dy):
                     attacks+=1
     if(attacks == 0):
+        print('\n')
         printBoard(chromosome, n_queen)
         print(chromosome)
-        STOP_FLAG = True
-    return (((n_queen-1)*n_queen)/2)-attacks
-    #return 28 - attacks
+        return True, (((n_queen-1)*n_queen)/2)-attacks
+    
+    return False, (((n_queen-1)*n_queen)/2)-attacks
 
+#create a population
 def getPopulation(size, n_queen):
     population = []
     for _ in range(size):
-        population.append(random.sample(range(1,n_queen+1), n_queen))
+        #population.append(random.sample(range(1,n_queen+1), n_queen))
+        population.append([random.randint(1,n_queen) for _ in range(n_queen)])
     return population
 
+#reproduce two individuals and return a child
 def reproduce(x,y, n_queen):
     c = random.randint(0,n_queen-1)
     child = x[0:c]+y[c:n_queen]
     return child
 
+#performs a mutation with a small probability
 def mutate(child, mutprob, n_queen):
     for i in range(len(child)):
         if(random.random() < mutprob):
             child[i] = random.randint(1,n_queen)
 
+#function to print the board
 def printBoard(chromosome, n_queen):
     for i in reversed(range(n_queen)):
         for j in range(n_queen):
             if(chromosome[j] == i+1):
-                print("X ", end='')
+                print("\t X ", end='')
             else:
-                print(". ", end='')
+                print("\t . ", end='')
         print("\n")
 
+#getting arguments
 parser = argparse.ArgumentParser()
 parser.add_argument('--maxiter', type=int, default=100000)
 parser.add_argument('--popsize', type=int, default=10)
@@ -54,11 +59,27 @@ parser.add_argument('--nqueen', type=int, default=8)
 args = parser.parse_args()
 
 population = getPopulation(args.popsize,args.nqueen)
+best = None
+best_prob = 0
+best_find = False
 
+#----- Main Loop -----
 for i in range(args.maxiter):
-    fit_prob = [(fitness(i, args.nqueen)/len(population))*100 for i in population]
-    if(STOP_FLAG):
+    print('Iteration: ' + str(i+1), end="\r")
+    fit_prob = []
+    for k in population:
+        best_find, prob = fitness(k, args.nqueen)
+        prob = (prob/len(population))*100
+        fit_prob.append(prob)
+        if(prob > best_prob):
+            best = k
+            best_prob = prob
+        if(best_find):
+            best = None
+            break    
+    if(best_find):
         break
+
     new_pop = []
     for j in range(len(population)):
         x = random.choices(population, weights=fit_prob)[0]
@@ -68,4 +89,10 @@ for i in range(args.maxiter):
         child = reproduce(x,y,args.nqueen)
         mutate(child, args.mutprob,args.nqueen)
         new_pop.append(child)
+
     population = new_pop.copy()
+
+if(best!=None):
+    print('\n')
+    printBoard(best, args.nqueen)
+    print(best)
